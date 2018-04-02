216ed7a18c679e1a976c5b43556a8b5be77c15a8
xingyatong <384289339@qq.com>
2018-04-02 10:44:08 +0800

    Initial commit

---
+++ app/src/main/java/com/blue/mediaplayer/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.blue.mediaplayer.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by xingyatong on 2018/4/2.
+ */
+
+public class MainActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
